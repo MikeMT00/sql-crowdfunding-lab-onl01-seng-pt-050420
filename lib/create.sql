13c43bf83b3c8472f1c6544ec07ad0f90a3f642e
Michael Thomas <michaelthomas0611@gmail.com>
2020-07-05 01:45:17 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,7 +7,8 @@ CREATE TABLE users (
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
-  funding_goal REAL,
+  category TEXT,
+  funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT,
 );
